28dc30f26259b49ed0146b23ce8ab3dbc8d14fd4
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2025-10-31 13:27:20 -0400

    add sample bbox plotting from google

--- tt.py
+++ tt.py
@@ -69,3 +69,97 @@ class InferViewer:
         )
         widgets.interact(self.view_image_cb, index=slider)
 
+# @title Plotting Util
+
+# Get Noto JP font to display janapese characters
+!apt-get install fonts-noto-cjk  # For Noto Sans CJK JP
+
+#!apt-get install fonts-source-han-sans-jp # For Source Han Sans (Japanese)
+
+import json
+import random
+import io
+from PIL import Image, ImageDraw, ImageFont
+from PIL import ImageColor
+
+additional_colors = [colorname for (colorname, colorcode) in ImageColor.colormap.items()]
+
+
+# Based on https://colab.research.google.com/github/google-gemini/cookbook/blob/main/quickstarts/Spatial_understanding.ipynb#scrollTo=oxK0pycZm4AY
+def plot_bounding_boxes(im, bounding_boxes):
+    """
+    Plots bounding boxes on an image with markers for each a name, using PIL, normalized coordinates, and different colors.
+
+    Args:
+        img_path: The path to the image file.
+        bounding_boxes: A list of bounding boxes containing the name of the object
+         and their positions in normalized [y1 x1 y2 x2] format.
+    """
+
+    # Load the image
+    img = im
+    width, height = img.size
+    print(img.size)
+    # Create a drawing object
+    draw = ImageDraw.Draw(img)
+
+    # Define a list of colors
+    colors = [
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'orange',
+    'pink',
+    'purple',
+    'brown',
+    'gray',
+    'beige',
+    'turquoise',
+    'cyan',
+    'magenta',
+    'lime',
+    'navy',
+    'maroon',
+    'teal',
+    'olive',
+    'coral',
+    'lavender',
+    'violet',
+    'gold',
+    'silver',
+    ] + additional_colors
+
+    # Parsing out the markdown fencing
+    bounding_boxes = parse_json(bounding_boxes)
+
+    font = ImageFont.truetype("NotoSansCJK-Regular.ttc", size=14)
+
+    # Iterate over the bounding boxes
+    for i, bounding_box in enumerate(json.loads(bounding_boxes)):
+      # Select a color from the list
+      color = colors[i % len(colors)]
+
+      # Convert normalized coordinates to absolute coordinates
+      abs_y1 = int(bounding_box["box_2d"][0]/1000 * height)
+      abs_x1 = int(bounding_box["box_2d"][1]/1000 * width)
+      abs_y2 = int(bounding_box["box_2d"][2]/1000 * height)
+      abs_x2 = int(bounding_box["box_2d"][3]/1000 * width)
+
+      if abs_x1 > abs_x2:
+        abs_x1, abs_x2 = abs_x2, abs_x1
+
+      if abs_y1 > abs_y2:
+        abs_y1, abs_y2 = abs_y2, abs_y1
+
+      # Draw the bounding box
+      draw.rectangle(
+          ((abs_x1, abs_y1), (abs_x2, abs_y2)), outline=color, width=4
+      )
+
+      # Draw the text
+      if "label" in bounding_box:
+        draw.text((abs_x1 + 8, abs_y1 + 6), bounding_box["label"], fill=color, font=font)
+
+    # Display the image
+    img.show()
